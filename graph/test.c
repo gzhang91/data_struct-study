e067e28b1507eb30f64810cdd034d64b5e2bad1e
gzhang <gzhang>
2020-12-31 18:01:19 +0800

    1. 整理insert

--- graph/test.c
+++ graph/test.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "graph.h"
 
+#define INSERT_EDGE(g, bv, ev, wv) \
+	e.start = bv;  \
+	e.end = ev;    \
+	e.weight = wv; \
+	GraphInsertEdge(g, e);
+
 int main(void) {
 
 	Graph *g = GraphInit(0, 5);
@@ -13,36 +19,13 @@ int main(void) {
 
 	Edge e = { 'A', 'B', 1 };
 	GraphInsertEdge(g, e);
+	INSERT_EDGE(g, 'A', 'C', 1);
+	INSERT_EDGE(g, 'A', 'D', 1);
+	INSERT_EDGE(g, 'A', 'E', 1);
+	INSERT_EDGE(g, 'B', 'C', 1);
+	INSERT_EDGE(g, 'C', 'D', 1);
+	INSERT_EDGE(g, 'D', 'E', 1);
 
-	e.start = 'A';
-	e.end = 'C';
-	e.weight = 1;
-	GraphInsertEdge(g, e);
-
-	e.start = 'A';
-	e.end = 'D';
-	e.weight = 1;
-	GraphInsertEdge(g, e);
-
-	e.start = 'A';
-	e.end = 'E';
-	e.weight = 1;
-	GraphInsertEdge(g, e);
-
-	e.start = 'B';
-	e.end = 'C';
-	e.weight = 1;
-	GraphInsertEdge(g, e);
-
-	e.start = 'C';
-	e.end = 'D';
-	e.weight = 1;
-	GraphInsertEdge(g, e);
-
-	e.start = 'D';
-	e.end = 'E';
-	e.weight = 1;
-	GraphInsertEdge(g, e);
 	GraphShow(g);
 	GraphDestroy(g);
 
